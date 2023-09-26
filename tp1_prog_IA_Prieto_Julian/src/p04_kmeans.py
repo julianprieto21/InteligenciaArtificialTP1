497079460555c84c97646f099fad11d54b1cdc7a
Julian Prieto <prietojulian2003@gmail.com>
2023-09-26 20:53:38 -0300

    Punto 4: Codigo casi terminado, falta punto b.

--- tp1_prog_IA_Prieto_Julian/src/p04_kmeans.py
+++ tp1_prog_IA_Prieto_Julian/src/p04_kmeans.py
@@ -1,9 +1,11 @@
-from matplotlib.image import imread
+from matplotlib.image import imread, imsave
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import pandas as pd
 import numpy as np
+from datetime import datetime
+import os
 
 A_large = imread("data\peppers-large.tiff")
 A_small = imread("data\peppers-small.tiff")
@@ -73,6 +75,8 @@ def distancia_euclideana(a, b):
 
 
 def k_means(data, k, max_iter=30):
+    inicio = datetime.now()
+
     centroides = data[
         np.random.choice(len(data), k, replace=False)
     ]  # Inicializo los valores de los centroides con pixeles al azar
@@ -89,26 +93,51 @@ def k_means(data, k, max_iter=30):
             break
 
         centroides = new_centroides
-
+    print(f"El modelo se entrenó en: {datetime.now() - inicio}")
     return centroides, etiquetas
+
+
+def comprimir_image(imagen, centroides):
+    data = np.array(imagen).reshape(-1, 3)  # Paso la imagen a una matriz de pixeles
+    etiquetas = np.ones(len(data), dtype=int)  # Inicializo las etiquetas de los pixeles
+    for i in range(len(data)):
+        distancia = 999  # Inicializo la distancia más grande
+        for j in range(len(centroides)):
+            distancia_temp = distancia_euclideana(
+                data[i], centroides[j]
+            )  # Calculo la distancia entre el pixel y el centroide
+            if (
+                distancia_temp < distancia
+            ):  # Si la distancia es menor que la anterior, actualizo la distancia y la etiqueta
+                distancia = distancia_temp
+                etiquetas[i] = j
+
+    data_comprimida = centroides[
+        etiquetas
+    ]  # Obtengo los pixeles de la nueva data comprimida
+    imagen_comprimida = data_comprimida.reshape(512, 512, 3).astype(
+        np.uint8
+    )  # Paso la data comprimida a una nueva imagen
+    return imagen_comprimida
 
 
 data = np.array(A_small).reshape(-1, 3)
 k = 16
 centroides, etiquetas = k_means(data, k)
+new_image = comprimir_image(A_large, centroides)
 
-data_comprimida = centroides[etiquetas]
+imsave("data/peppers-compressed.png", new_image)
 
-imagen_comprimida = data_comprimida.reshape(128, 128, 3).astype(np.uint8)
-
-print(centroides)
-
-# plt.subplot(1, 2, 1)
-# plt.title("Imagen Original")
-# plt.imshow(A_small)
-# plt.axis("off")
-# plt.subplot(1, 2, 2)
-# plt.title("Imagen Comprimida")
-# plt.imshow(imagen_comprimida.astype(np.uint8))
-# plt.axis("off")
-# plt.show()
+plt.subplot(1, 2, 1)
+plt.title(
+    f"Imagen Original: {os.path.getsize('data/peppers-large.tiff') / 1024:.2f} KB"
+)
+plt.imshow(A_small)
+plt.axis("off")
+plt.subplot(1, 2, 2)
+plt.title(
+    f"Imagen Comprimida: {os.path.getsize('data/peppers-compressed.png') / 1024:.2f} KB"
+)
+plt.imshow(new_image)
+plt.axis("off")
+plt.show()
